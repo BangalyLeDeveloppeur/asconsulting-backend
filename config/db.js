6e4adb3af72f5b5e61d385831cddcf64c7c0921f
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2026-03-12 23:11:25 +0100

    unitialise les variable environnement pour mysql

--- config/db.js
+++ config/db.js
@@ -1,15 +1,18 @@
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "asconsulting",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "asconsulting",
   port: 3306,
 });
 
 db.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error("❌ Erreur de connexion MySQL :", err.message);
+    return;
+  }
   console.log("✅ Connecté à MySQL");
 });
 
