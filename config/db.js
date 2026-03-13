2b3e78ee6a3bbdcb6941d97f014eef570051c127
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2026-03-13 17:59:44 +0100

    fix: mysql createpool au lieu de createConnection

--- config/db.js
+++ config/db.js
@@ -1,24 +1,26 @@
 import mysql from "mysql2";
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "asconsulting",
-  port: 3306,
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("❌ Erreur de connexion MySQL :", {
       message: err.message,
       code: err.code,
-      errno: err.errno,
-      sqlState: err.sqlState
     });
     return;
   }
-  console.log("✅ Connecté à MySQL");
+  console.log("✅ Connecté à MySQL !");
+  connection.release();
 });
 
-export default db;
+export default pool;
